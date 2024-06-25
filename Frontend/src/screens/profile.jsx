6507ed6eed181f653940cfb0bcb1d924d34e7bf7
Cristopher <cristopher.meneses@utec.edu.pe>
2024-06-24 22:43:06 -0500

    ahora si

--- Frontend/src/screens/profile.jsx
+++ Frontend/src/screens/profile.jsx
@@ -1,59 +1,154 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView, Dimensions, TouchableOpacity, Modal, TextInput, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import profileImage from 'C:/Users/crist/fitlendar/fitlendarFrontend/Frontend/assets/hombre.png'; // Ajusta la ruta según sea necesario
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; // Importa el icono
+
+const { width, height } = Dimensions.get('window');
+// Asegúrate de actualizar la ruta de la imagen
+import profileImage from 'C:/Users/crist/fitlendar/fitlendarFrontend/Frontend/assets/hombre.png';
 
 export default function Profile() {
+    const [profile, setProfile] = useState({
+        altura: '',
+        nombre: '',
+        peso: '',
+        foto: ''
+       
+    });
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [editableProfile, setEditableProfile] = useState({ ...profile });
+
+    useEffect(() => {
+        fetchUserData();
+    }, []);
+
+    const fetchUserData = async () => {
+        try {
+            const response = await fetch('http://Fitlendar-LB-914168723.us-east-1.elb.amazonaws.com:8002/users/juancitotuviejo@gmail.com', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+    
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+    
+            const contentType = response.headers.get('content-type');
+            if (!contentType || !contentType.includes('application/json')) {
+                throw new Error('Received non-JSON response from server');
+            }
+    
+            const data = await response.json();
+            setProfile({
+                altura: data.altura,
+                nombre: data.nombre,
+                peso: data.peso,
+                foto: data.foto
+            });
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    };
+    
+
+    const saveProfile = async () => {
+        await updateUserData();
+        setModalVisible(false);
+    };
+
+    const updateUserData = async () => {
+        try {
+            const response = await fetch('http://Fitlendar-LB-914168723.us-east-1.elb.amazonaws.com:8002/users/juancitotuviejo@gmail.com', { // Asegúrate de tener esta ruta configurada en Flask
+                method: 'PATCH', // o POST si así lo manejas
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(editableProfile)
+            });
+            const data = await response.json();
+            if (response.ok) {
+                setProfile(editableProfile);
+                alert('Datos actualizados correctamente');
+            } else {
+                console.error(data.error);
+            }
+        } catch (error) {
+            console.error('Error updating user data:', error);
+        }
+    };
+
     return (
-        <SafeAreaView style={styles.safeArea} edges={['top']}>
+        <SafeAreaView style={styles.safeArea}>
             <StatusBar style="light" />
-            {/* punchline and avatar */}
-            <View style={styles.container}>
-                <View style={styles.textContainer}>
-                    <Text style={[styles.text, styles.textNeutral]}>READY TO GO</Text>
-                    <Text style={[styles.text, styles.textRose]}>CRISTOPHER</Text>
-                </View>
-                <Image source={profileImage} style={styles.avatar} />
-            </View>
-            {/* Cuadros para los datos */}
-            <View style={styles.statsContainer}>
-                <View style={styles.statBox}>
-                    <Text style={styles.statText}>Peso</Text>
-                    <Text style={styles.statValue}>70 kg</Text>
-                </View>
-                <View style={styles.statBox}>
-                    <Text style={styles.statText}>Talla</Text>
-                    <Text style={styles.statValue}>175 cm</Text>
-                </View>
-                <View style={styles.statBox}>
-                    <Text style={styles.statText}>Calorías</Text>
-                    <Text style={styles.statValue}>2000 kcal</Text>
-                </View>
-                <View style={styles.statBox}>
-                    <Text style={styles.statText}>Horas</Text>
-                    <Text style={styles.statValue}>8 h</Text>
-                </View>
-            </View>
-            {/* Actividades */}
-            <View style={styles.activitiesContainer}>
-                <Text style={styles.activitiesTitle}>Today</Text>
-                <View style={styles.activityRow}>
-                    <View style={styles.activity}>
-                        <Image source={require('C:/Users/crist/fitlendar/fitlendarFrontend/Frontend/assets/gym1.jpeg')} style={styles.activityImage} />
-                        <Text style={styles.activityText}>Bench Press</Text>
+            <ScrollView style={styles.scrollView}>
+                <View style={styles.profileHeader}>
+                    <Image source={profile.foto} style={styles.profileImage} />
+                    <View style={styles.profileInfo}>
+                        <Text style={styles.profileName}>Hi, {profile.nombre}</Text>
+                        <View style={styles.beginnerBar}>
+                            <Text style={styles.beginnerText}>Beginner - 120 hrs</Text>
+                        </View>
                     </View>
-                    <View style={styles.activity}>
-                        <Image source={require('C:/Users/crist/fitlendar/fitlendarFrontend/Frontend/assets/biceps.jpeg')} style={styles.activityImage} />
-                        <Text style={styles.activityText}>Biceps</Text>
+                    <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.editButton}>
+                        <Icon name="pencil" size={24} color="#FFFFFF" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.statsContainer}>
+                    <View style={styles.statBox}>
+                        <Text style={styles.statText}>Peso</Text>
+                        <Text style={styles.statValue}>{profile.peso}</Text>
                     </View>
-                    <View style={styles.activity}>
-                        <Image source={require('C:/Users/crist/fitlendar/fitlendarFrontend/Frontend/assets/gym2.jpeg')} style={styles.activityImage} />
-                        <Text style={styles.activityText}>Running</Text>
+                    <View style={styles.statBox}>
+                        <Text style={styles.statText}>Talla</Text>
+                        <Text style={styles.statValue}>{profile.altura}</Text>
+                    </View>
+                    <View style={styles.statBox}>
+                        <Text style={styles.statText}>Calorías</Text>
+                        <Text style={styles.statValue}>{profile.calories}</Text>
+                    </View>
+                    <View style={styles.statBox}>
+                        <Text style={styles.statText}>Horas</Text>
+                        <Text style={styles.statValue}>{profile.hours}</Text>
                     </View>
                 </View>
-            </View>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => setModalVisible(false)}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={(text) => setEditableProfile({ ...editableProfile, nombre: text })}
+                                value={editableProfile.nombre}
+                                placeholder="Nombre"
+                            />
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={(text) => setEditableProfile({ ...editableProfile, peso: text ? Number(text) : '' })}
+                                value={editableProfile.peso.toString()}
+                                placeholder="Peso"
+                                keyboardType="numeric"
+                            />  
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={(text) => setEditableProfile({ ...editableProfile, altura: text ? Number(text) : '' })}
+                                value={editableProfile.altura.toString()}
+                                placeholder="Altura"
+                                keyboardType="numeric"
+                            />
+                                                        <Button title="Guardar Cambios" onPress={saveProfile} />
+                            <Button title="Cancelar" color="#FF6347" onPress={() => setModalVisible(false)} />
+                        </View>
+                    </View>
+                </Modal>
+            </ScrollView>
         </SafeAreaView>
     );
 }
@@ -61,94 +156,98 @@ export default function Profile() {
 const styles = StyleSheet.create({
     safeArea: {
         flex: 1,
-        backgroundColor: '#1C1C1E', // Negro premium de Apple
+        backgroundColor: '#1C1C1E',
     },
-    container: {
+    scrollView: {
+        marginHorizontal: 10,
+    },
+    profileHeader: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
         alignItems: 'center',
-        marginHorizontal: wp(5),
+        justifyContent: 'space-between',
+        marginTop: 20,
+        paddingHorizontal: 10,
     },
-    avatar: {
-        width: hp(10), // Ajusta el tamaño según sea necesario
-        height: hp(10), // Ajusta el tamaño según sea necesario
-        borderRadius: hp(5), // La mitad del ancho y alto para hacerla circular
-        borderWidth: 4,
-        borderColor: '#FFFFFF',
-        marginLeft: wp(5),
+    profileImage: {
+        width: width * 0.2,
+        height: width * 0.2,
+        borderRadius: width * 0.1,
     },
-    textContainer: {
-        marginVertical: hp(4),
+    profileInfo: {
+        flex: 1,
+        marginLeft: 20,
     },
-    text: {
-        fontSize: hp(4),
+    profileName: {
+        fontSize: 24,
         fontWeight: 'bold',
-        letterSpacing: 1,
+        color: '#FFFFFF',
     },
-    textNeutral: {
-        color: '#FFFFFF', // Color blanco para visibilidad en fondo negro
+    beginnerBar: {
+        backgroundColor: '#474B4E',
+        borderRadius: 10,
+        padding: 5,
+        marginTop: 5,
     },
-    textRose: {
-        color: '#FF3B30', // Color rojo de Apple
+    beginnerText: {
+        fontSize: 16,
+        color: '#FFFFFF',
+    },
+    editButton: {
+        padding: 10,
     },
     statsContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
         flexWrap: 'wrap',
-        margin: wp(5),
+        marginTop: 20,
     },
     statBox: {
-        width: wp(40),
-        height: hp(10),
+        width: width * 0.4,
+        height: height * 0.1,
         borderWidth: 1,
-        borderColor: '#2C2C2E',
+        borderColor: '#ccc',
         backgroundColor: '#2C2C2E',
-        borderRadius: 17,
+        borderRadius: 10,
         justifyContent: 'center',
         alignItems: 'center',
-        marginVertical: hp(1),
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.9,
-        shadowRadius: 4,
-        elevation: 5,
+        marginVertical: 10,
     },
     statText: {
         color: '#A0A0A0',
-        fontSize: hp(2),
+        fontSize: 16,
     },
     statValue: {
         color: '#FFFFFF',
-        fontSize: hp(2.5),
+        fontSize: 20,
         fontWeight: 'bold',
     },
-    activitiesContainer: {
-        marginTop: hp(4),
-        marginHorizontal: wp(5),
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
-    activitiesTitle: {
-        color: '#FFFFFF',
-        fontSize: hp(3),
-        fontWeight: 'bold',
-        marginBottom: hp(3),
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
     },
-    activityRow: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    activity: {
-        alignItems: 'center',
-    },
-    activityImage: {
-        width: wp(20),
-        height: wp(20),
-        borderRadius: wp(10),
-        marginBottom: hp(1),
-        borderWidth: 2,
-        borderColor: '#FFFFFF',
-    },
-    activityText: {
-        color: '#A0A0A0',
-        fontSize: hp(2),
-    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        width: 200,
+        marginBottom: 20,
+    }
 });
