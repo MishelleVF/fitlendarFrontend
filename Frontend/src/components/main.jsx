bccc3ddcf763b5fd8d2edef2937f7c7ec842cf56
mishellevf <mishelle.villarreal@utec.edu.pe>
2024-07-03 20:28:59 -0500

    a

--- Frontend/src/components/main.jsx
+++ Frontend/src/components/main.jsx
@@ -1,8 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { TabNavigator } from "./tabNavigator";
-import { Login } from "../screens/login";
-import { login2 } from "../estilos/estilos";
 import Login2 from "../screens/login2";
 import { Crear1 } from "../screens/crearcuenta";
 import { GoogleCalendarScreen } from '../screens/GoogleCalendarScreen';
@@ -12,7 +10,7 @@ const Stack = createStackNavigator();
 export function Main() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="GoogleCalendar">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login2} />
         <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }} />
         <Stack.Screen name="Signup" component={Crear1} />
