cfd85f6fc7be2292a00fdc375fab39bc1db877a3
Cristopher <cristopher.meneses@utec.edu.pe>
2024-07-09 20:34:29 -0500

    djdj

--- Frontend/src/screens/profile.jsx
+++ Frontend/src/screens/profile.jsx
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, ScrollView, Dimensions, TouchableOpacity, Modal, TextInput, Button } from 'react-native';
+import {
+    View, Text, StyleSheet, Image, ScrollView, Dimensions, TouchableOpacity, Modal, TextInput, Button, Alert
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; // Importa el icono
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles from '../estilos/profileStyle';
 
 const { width, height } = Dimensions.get('window');
-// Asegúrate de actualizar la ruta de la imagen
-
 
 export default function Profile() {
     const [profile, setProfile] = useState({
         altura: '',
         nombre: '',
         peso: '',
-        foto: ''
-       
+        foto: '',
+        racha: '',
+        calorias_quemadas: ''
     });
 
     const [modalVisible, setModalVisible] = useState(false);
@@ -27,59 +28,54 @@ export default function Profile() {
 
     const fetchUserData = async () => {
         try {
-            
-            const response = await fetch('http://fitlendar-lb-1465450486.us-east-1.elb.amazonaws.com:8002/users/nuevousuario@gmail.com', {
+            const response = await fetch('http://fitlendar-lb-1465450486.us-east-1.elb.amazonaws.com:8002/users/meneses@gmail.com', {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json'
                 }
             });
-    
+
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-    
-            const contentType = response.headers.get('content-type');
-            if (!contentType || !contentType.includes('application/json')) {
-                throw new Error('Received non-JSON response from server');
-            }
-    
+
             const data = await response.json();
-            setProfile({
-                altura: data.altura,
-                nombre: data.nombre,
-                peso: data.peso,
-                foto: data.foto
-            });
+            setProfile(data);
         } catch (error) {
             console.error('Error fetching user data:', error);
         }
     };
-    
 
     const saveProfile = async () => {
+        if (!editableProfile.nombre.trim() || editableProfile.peso <= 0 || editableProfile.altura <= 0) {
+            Alert.alert('Error', 'Please enter all fields correctly.');
+            return;
+        }
+
         await updateUserData();
         setModalVisible(false);
     };
 
     const updateUserData = async () => {
         try {
-            const response = await fetch('http://fitlendar-lb-1465450486.us-east-1.elb.amazonaws.com:8002/users/nuevousuario@gmail.com', { // Asegúrate de tener esta ruta configurada en Flask
-                method: 'PATCH', // o POST si así lo manejas
+            const response = await fetch('http://fitlendar-lb-1465450486.us-east-1.elb.amazonaws.com:8002/users/meneses@gmail.com', {
+                method: 'PATCH',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(editableProfile)
             });
-            const data = await response.json();
-            if (response.ok) {
-                setProfile(editableProfile);
-                alert('Datos actualizados correctamente');
-            } else {
-                console.error(data.error);
+
+            if (!response.ok) {
+                throw new Error('Failed to update user data');
             }
+
+            const data = await response.json();
+            setProfile(editableProfile);
+            Alert.alert('Success', 'Profile updated successfully');
         } catch (error) {
             console.error('Error updating user data:', error);
+            Alert.alert('Error', 'Failed to update profile');
         }
     };
 
@@ -88,11 +84,10 @@ export default function Profile() {
             <StatusBar style="light" />
             <ScrollView style={styles.scrollView}>
                 <View style={styles.profileHeader}>
-                    <Image source={{ uri: profile.foto }} style={styles.profileImage} />
+                    <Image source={{ uri: profile.foto }} style={styles.profileImage} />
                     <View style={styles.profileInfo}>
                         <Text style={styles.profileName}>Hi, {profile.nombre}</Text>
-                        <Text style={styles.profiletext}>READY TO GO!!</Text>
-                        
+                        <Text style={styles.profileText}>READY TO GO!!</Text>
                     </View>
                     <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.editButton}>
                         <Icon name="pencil" size={24} color="#FFFFFF" />
@@ -109,11 +104,11 @@ export default function Profile() {
                     </View>
                     <View style={styles.statBox}>
                         <Text style={styles.statText}>Calorías</Text>
-                        <Text style={styles.statValue}>{profile.calories}</Text>
+                        <Text style={styles.statValue}>{profile.calorias_quemadas}</Text>
                     </View>
                     <View style={styles.statBox}>
-                        <Text style={styles.statText}>Horas</Text>
-                        <Text style={styles.statValue}>{profile.hours}</Text>
+                        <Text style={styles.statText}>Racha</Text>
+                        <Text style={styles.statValue}>{profile.racha}</Text>
                     </View>
                 </View>
                 <Modal
@@ -136,7 +131,7 @@ export default function Profile() {
                                 value={editableProfile.peso.toString()}
                                 placeholder="Peso"
                                 keyboardType="numeric"
-                            />  
+                            />
                             <TextInput
                                 style={styles.input}
                                 onChangeText={(text) => setEditableProfile({ ...editableProfile, altura: text ? Number(text) : '' })}
@@ -144,7 +139,7 @@ export default function Profile() {
                                 placeholder="Altura"
                                 keyboardType="numeric"
                             />
-                                                        <Button title="Guardar Cambios" onPress={saveProfile} />
+                            <Button title="Guardar Cambios" onPress={saveProfile} />
                             <Button title="Cancelar" color="#FF6347" onPress={() => setModalVisible(false)} />
                         </View>
                     </View>
